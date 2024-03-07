19a64057ad3b1e6eef92fad641ba895e057971d3
alan69104 <160734352+alan69104@users.noreply.github.com>
2024-03-07 22:39:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
 import time
-import os
-
-# 獲取環境變量中Chrome WebDriver的路徑
-chrome_driver_path = os.path.join(os.getenv('PATH'), '/opt/render/project/.render/chrome/opt/google/chrome/')
 
 # 使用 Chrome WebDriver
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver = webdriver.Chrome()
 
 # 打開網頁
 driver.get("https://www.example.com")
