9ab802a94396a1f0a86200810a4f25f846d3dfdc
alan69104 <160734352+alan69104@users.noreply.github.com>
2024-03-07 19:55:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 
 # 建立一個Firefox的webdriver物件，並指定driver的路徑
-browser = webdriver.Chrome(executable_path="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/")
+browser = webdriver.Chrome(executable_path=${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/)
 
 # 連到我的網站
 browser.get("https://kirin.idv.tw")
