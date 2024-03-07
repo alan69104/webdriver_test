c8d3302591e8b6df8bbd5c18b369904818f4514c
alan69104 <160734352+alan69104@users.noreply.github.com>
2024-03-07 20:11:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 # 載入需要的套件
+from flask import Flask
 from selenium import webdriver
+import os
 
-# 建立一個Firefox的webdriver物件，並指定driver的路徑
-browser = webdriver.Chrome(executable_path=${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/)
+app = Flask(__name__)
 
-# 連到我的網站
-browser.get("https://kirin.idv.tw")
+@app.route('/')
+def index():
+    # 建立一個Chrome的webdriver物件，並指定driver的路徑
+    browser = webdriver.Chrome(executable_path=os.environ['PATH'] + ":/opt/render/project/.render/chrome/opt/google/chrome/")
 
-# 印出網頁的標題
-print(browser.title)
+    # 連到我的網站
+    browser.get("https://kirin.idv.tw")
 
-# 關閉瀏覽器
-browser.quit()
+    # 印出網頁的標題
+    title = browser.title
+
+    # 關閉瀏覽器
+    browser.quit()
+
+    return title
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
