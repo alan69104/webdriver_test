ab7741f7141d29a86bd6d03a2869ad0e25e0b1bf
alan69104 <160734352+alan69104@users.noreply.github.com>
2024-03-10 17:20:21 +0800

    Update render-start.sh

--- render-start.sh
+++ render-start.sh
@@ -11,13 +11,13 @@ function log_error {
 
 echo "開始部署..."
 
-# 下載並解壓 Chrome
+# 定義 Chrome 的下載 URL
 CHROME_DOWNLOAD_URL="https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chrome-linux64.zip"
 echo "...正在下載指定版本的 Chrome"
 mkdir -p $STORAGE_DIR/chrome
 cd $STORAGE_DIR/chrome
 wget -nv "$CHROME_DOWNLOAD_URL" || log_error "Chrome 下載失敗"
-unzip -q chrome-linux64.zip || log_error "解壓失敗"
+unzip -o -q chrome-linux64.zip || log_error "解壓失敗" # 使用 -o 選項自動覆蓋檔案
 rm chrome-linux64.zip
 cd - # 返回之前的目錄
 
