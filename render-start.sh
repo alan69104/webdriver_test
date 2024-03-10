e58bb5067fb3b9cbbb9723f5d964608ca6a91def
alan69104 <160734352+alan69104@users.noreply.github.com>
2024-03-10 15:44:37 +0800

    Update render-start.sh

--- render-start.sh
+++ render-start.sh
@@ -5,36 +5,20 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-    echo "正在下载 Chrome..."
-    mkdir -p $STORAGE_DIR/chrome
-    cd $STORAGE_DIR/chrome
-    wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-    rm ./google-chrome-stable_current_amd64.deb
-    cd $HOME/project/src # 确保我们返回到原本的位置
-    echo "Chrome 安装完成。"
-else
-    echo "从缓存中使用 Chrome。"
-fi
+# ... [省略之前的 Chrome 安装步骤] ...
 
-# 设置 Chrome 可执行文件的目录到环境变量 PATH
-CHROME_DIR="/opt/render/project/.render/chrome/opt/google/chrome"
-export PATH="$PATH:$CHROME_DIR"
-
-# 调试信息
-echo "CHROME_DIR 设置为: $CHROME_DIR"
-ls -l "$CHROME_DIR/chrome"
-which chrome
-
-# 获取 Chrome 版本
+# 获取 Chrome 主版本号
 CHROME_VERSION=$(google-chrome --product-version | cut -d '.' -f 1-3)
 echo "Chrome 版本: $CHROME_VERSION"
+
+# 获取与 Chrome 兼容的 ChromeDriver 最新版本号
+CHROMEDRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+echo "ChromeDriver 兼容版本: $CHROMEDRIVER_VERSION"
 
 # 安装 ChromeDriver
 if [[ ! -f $STORAGE_DIR/chromedriver ]]; then
     echo "正在下载 ChromeDriver..."
-    wget -P $STORAGE_DIR https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
+    wget -P $STORAGE_DIR "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
     unzip $STORAGE_DIR/chromedriver_linux64.zip -d $STORAGE_DIR
     rm $STORAGE_DIR/chromedriver_linux64.zip
     echo "ChromeDriver 安装完成。"
@@ -42,14 +26,4 @@ else
     echo "从缓存中使用 ChromeDriver。"
 fi
 
-# 设置 ChromeDriver 执行路径到环境变量
-CHROME_DRIVER_PATH="$STORAGE_DIR"
-export PATH="$PATH:$CHROME_DRIVER_PATH"
-
-# 调试信息
-echo "CHROME_DRIVER_PATH 设置为: $CHROME_DRIVER_PATH"
-ls -l "$CHROME_DRIVER_PATH/chromedriver"
-which chromedriver
-
-mkdir -p /tmp
-# 在此添加您自己的构建命令...
+# ... [省略之后的步骤] ...
