cab8207cf2ae6b8e8c76f8dcc5374f01fbc14f6b
alan69104 <160734352+alan69104@users.noreply.github.com>
2024-03-10 18:18:33 +0800

    Update render-start.sh

--- render-start.sh
+++ render-start.sh
@@ -8,19 +8,15 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -nv -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  echo "...Extracting Chrome package"
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  # Find the version of the extracted Chrome
-  CHROME_VERSION=$(dpkg-deb -f ./google-chrome-stable_current_amd64.deb Version)
-  echo "Installed Chrome version: $CHROME_VERSION"
-  cd - # Make sure we return to where we were
+  cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
 fi
 
-# be sure to add Chrome's location to the PATH as part of your Start Command
-export PATH="${PATH}:${STORAGE_DIR}/chrome/opt/google/chrome"
-
+# be sure to add Chromes location to the PATH as part of your Start Command
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+/opt/render/project/.render/chrome/opt/google/chrome/chrome --version
 # add your own build commands...
