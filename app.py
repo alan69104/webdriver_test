383534390e4d987587996e8c085d368a57be70e4
alan69104 <160734352+alan69104@users.noreply.github.com>
2024-03-11 21:09:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def index():
     # Set up Chrome options
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
     chrome_options.binary_location = "/opt/render/project/.render/chrome/opt/google/chrome/chrome"  # Specify Chrome binary location
 
